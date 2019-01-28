912044985fb55058bc34b28bddad706670439aee
Peter Albanese <petera@albx.com>
2019-01-28 17:14:22 -0600

    Add project files.

---
+++ EmailTester/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace EmailTester
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtHost = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.chkUseSSL = new System.Windows.Forms.CheckBox();
+            this.cmdSend = new System.Windows.Forms.Button();
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtSubject = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtFromEmail = new System.Windows.Forms.TextBox();
+            this.cmdCopy = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtHost
+            // 
+            this.txtHost.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.txtHost.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHost.Location = new System.Drawing.Point(47, 52);
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(269, 26);
+            this.txtHost.TabIndex = 0;
+            this.txtHost.Text = "smtp.office365.com";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.txtUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsername.Location = new System.Drawing.Point(47, 105);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(413, 26);
+            this.txtUsername.TabIndex = 1;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(47, 158);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(413, 26);
+            this.txtPassword.TabIndex = 2;
+            // 
+            // txtPort
+            // 
+            this.txtPort.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.txtPort.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPort.Location = new System.Drawing.Point(332, 52);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(128, 26);
+            this.txtPort.TabIndex = 3;
+            this.txtPort.Text = "587";
+            // 
+            // chkUseSSL
+            // 
+            this.chkUseSSL.AutoSize = true;
+            this.chkUseSSL.Checked = true;
+            this.chkUseSSL.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkUseSSL.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkUseSSL.Location = new System.Drawing.Point(484, 54);
+            this.chkUseSSL.Name = "chkUseSSL";
+            this.chkUseSSL.Size = new System.Drawing.Size(92, 24);
+            this.chkUseSSL.TabIndex = 4;
+            this.chkUseSSL.Text = "Use SSL";
+            this.chkUseSSL.UseVisualStyleBackColor = true;
+            // 
+            // cmdSend
+            // 
+            this.cmdSend.Location = new System.Drawing.Point(47, 504);
+            this.cmdSend.Name = "cmdSend";
+            this.cmdSend.Size = new System.Drawing.Size(269, 36);
+            this.cmdSend.TabIndex = 5;
+            this.cmdSend.Text = "Send Email";
+            this.cmdSend.UseVisualStyleBackColor = true;
+            this.cmdSend.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtTo
+            // 
+            this.txtTo.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.txtTo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTo.Location = new System.Drawing.Point(47, 294);
+            this.txtTo.Name = "txtTo";
+            this.txtTo.Size = new System.Drawing.Size(555, 26);
+            this.txtTo.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 278);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "To";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Host (mail.domain.com)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(332, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(128, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "SMTP Port (25, 465, 587)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(47, 89);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(227, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "SMTP Username (user OR user@domain.com)";
+            // 
+            // txtSubject
+            // 
+            this.txtSubject.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSubject.Location = new System.Drawing.Point(47, 349);
+            this.txtSubject.Name = "txtSubject";
+            this.txtSubject.Size = new System.Drawing.Size(555, 26);
+            this.txtSubject.TabIndex = 11;
+            this.txtSubject.Text = "Test Email Subject";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(47, 333);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Subject Line";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMessage.Location = new System.Drawing.Point(47, 399);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(555, 77);
+            this.txtMessage.TabIndex = 13;
+            this.txtMessage.Text = "This is a test email sent from the Email Tester application.";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(47, 383);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(77, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Message Body";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(47, 221);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(155, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "From Email (user@domain.com)";
+            // 
+            // txtFromEmail
+            // 
+            this.txtFromEmail.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.txtFromEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFromEmail.Location = new System.Drawing.Point(47, 237);
+            this.txtFromEmail.Name = "txtFromEmail";
+            this.txtFromEmail.Size = new System.Drawing.Size(413, 26);
+            this.txtFromEmail.TabIndex = 15;
+            // 
+            // cmdCopy
+            // 
+            this.cmdCopy.Location = new System.Drawing.Point(466, 238);
+            this.cmdCopy.Name = "cmdCopy";
+            this.cmdCopy.Size = new System.Drawing.Size(136, 26);
+            this.cmdCopy.TabIndex = 17;
+            this.cmdCopy.Text = "Copy from Smtp User";
+            this.cmdCopy.UseVisualStyleBackColor = true;
+            this.cmdCopy.Click += new System.EventHandler(this.cmdCopy_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(47, 142);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(86, 13);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "SMTP Password";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(677, 562);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.cmdCopy);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtFromEmail);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtSubject);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTo);
+            this.Controls.Add(this.cmdSend);
+            this.Controls.Add(this.chkUseSSL);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.txtHost);
+            this.Name = "Form1";
+            this.Text = "Email Tester (ALL FIELDS ARE REQUIRED)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtHost;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.CheckBox chkUseSSL;
+        private System.Windows.Forms.Button cmdSend;
+        private System.Windows.Forms.TextBox txtTo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSubject;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtFromEmail;
+        private System.Windows.Forms.Button cmdCopy;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ EmailTester/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmailTester
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+
+                this.cmdSend.Enabled = false;
+
+                String fromEmailAddress = txtFromEmail.Text;
+                String toEmailAddress = txtTo.Text;
+                String userName = txtUsername.Text;
+                String password = txtPassword.Text;
+                String host = txtHost.Text;
+                String subject = txtSubject.Text;
+                String body = txtMessage.Text;
+
+                int port = Convert.ToInt32(txtPort.Text);
+
+                MailMessage msg = new MailMessage();
+                msg.To.Add(new MailAddress(toEmailAddress));
+                msg.From = new MailAddress(fromEmailAddress);
+                msg.Subject = subject;
+                msg.Body = body;
+                msg.IsBodyHtml = true;
+
+                SmtpClient client = new SmtpClient();
+                client.Host = host;
+                client.Credentials = new System.Net.NetworkCredential(userName, password);
+                client.Port = Convert.ToInt32(port);
+                client.EnableSsl = chkUseSSL.Checked;
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.Send(msg);
+
+                MessageBox.Show("Email sent");
+
+                this.cmdSend.Enabled = true;
+
+            }
+
+            catch (SmtpException smtpEx)
+            {
+                var msg = $"SmtpException: {smtpEx.Message} - {smtpEx.InnerException.Message}";
+                MessageBox.Show(msg, "SmtpException");
+
+            }
+
+            catch (Exception ex)
+            {
+                var msg = $"Exception: {ex.Message}";
+                MessageBox.Show(msg, "Exception");
+            }
+
+            finally
+            {
+                this.cmdSend.Enabled = true;
+            }
+            
+
+        }
+
+        private void cmdCopy_Click(object sender, EventArgs e)
+        {
+
+            this.txtFromEmail.Text = this.txtUsername.Text;
+
+        }
+    }
+}
